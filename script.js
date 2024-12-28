31197514fff2733ac522f92113f78eb79c2cb921
nithya-c-c11 <nithyananda832851@gmail.com>
2024-12-28 13:01:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ displayEvents(events);
 // Theme toggle functionality
 const themeToggleButton = document.getElementById('theme-toggle');
 themeToggleButton.addEventListener('click', () => {
-    document.body.classList.toggle('light');
+    document.body.classList.toggle('dark-mode');
 });
 
 // Store event details in localStorage and navigate to event-detail.html
